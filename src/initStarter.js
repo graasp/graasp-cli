9704a1b74e51909320be78a2472cd101b8cffffa
Juan Carlos Farah <juancarlos.farah@epfl.ch>
2019-02-06 11:59:59 +0100

    feat: initialize a git repository when creating a new app
    closes #12

--- src/initStarter.js
+++ src/initStarter.js
@@ -7,9 +7,9 @@ import { sync as existsSync } from 'fs-exists-cached';
 import { DEFAULT_STARTER } from './config';
 
 // use execa to spawn a better child process
-const spawn = (cmd) => {
+const spawn = (cmd, opts = { stdio: 'inherit' }) => {
   const [file, ...args] = cmd.split(/\s+/);
-  return execa(file, args, { stdio: 'inherit' });
+  return execa(file, args, opts);
 };
 
 /**
@@ -42,6 +42,52 @@ const install = async (rootPath) => {
   }
 };
 
+// creates an new git repository
+const initGit = async (rootPath) => {
+  const prevDir = process.cwd();
+
+  console.log('creating new git repository...');
+  process.chdir(rootPath);
+
+  try {
+    execSync('git init', { stdio: 'ignore' });
+  } finally {
+    process.chdir(prevDir);
+  }
+};
+
+// perform initial commit
+const commit = async (rootPath) => {
+  const prevDir = process.cwd();
+
+  console.log('performing initial commit...');
+  process.chdir(rootPath);
+
+  try {
+    await spawn('git add -A', { stdio: 'ignore' });
+
+    // cannot spawn this because of the way we are splitting the command
+    execSync('git commit -m "chore: initial commit from graasp cli"', { stdio: 'ignore' });
+    console.log('performed initial commit');
+  } catch (e) {
+    // if we successfully initialized but couldn't commit, maybe the commit
+    // author config is not set, or the repository is being set in a directory
+    // that conflicts with our dependencies, e.g. inside a package that has a
+    // babel-eslint version incompatible with create-react-app, so if we get to
+    // this state, we remove the git files and show a message to the user
+    try {
+      console.log('could not initialize the git repository');
+      console.log('is your commit author config set?');
+      console.log('might you be running this command inside a package with babel-eslint?');
+      await fs.remove('.git');
+    } catch (removeErr) {
+      // ignore
+    }
+  } finally {
+    process.chdir(prevDir);
+  }
+};
+
 const clone = async (hostInfo, rootPath) => {
   let url;
   // let people use private repos accessed over ssh
@@ -62,7 +108,13 @@ const clone = async (hostInfo, rootPath) => {
 
   await fs.remove(path.join(rootPath, '.git'));
 
+  await initGit(rootPath);
+
+  console.log('initialized git repository');
+
   await install(rootPath);
+
+  await commit(rootPath);
 };
 
 const initStarter = async (projectDirectory, options = {}) => {
@@ -71,7 +123,12 @@ const initStarter = async (projectDirectory, options = {}) => {
   const { starter = DEFAULT_STARTER } = options;
 
   if (existsSync(path.join(rootPath, 'package.json'))) {
-    console.error(`directory ${rootPath} is already an npm project`);
+    console.error(`destination path '${rootPath}' is already an npm project`);
+    return false;
+  }
+
+  if (existsSync(path.join(rootPath, '.git'))) {
+    console.error(`destination path '${rootPath}' is already a git repository`);
     return false;
   }
 
