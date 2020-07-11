ad0f6f65941564988ca55ee0654eb1f969b0099e
Uchendu Nwachukwu <uchendu.nwachukwu@edu.hefr.ch>
2020-07-11 18:00:33 +0200

    chore: remove obsolete comment

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,7 @@
 import execa from 'execa';
 
-// TODO: this is not optimal naming since it may cause confusion with 'spawn()' from 'child_process' module;
-
 // use execa to spawn a better child process
-/* eslint-disable-next-line import/prefer-default-export */
+// eslint-disable-next-line import/prefer-default-export
 export const spawnProcess = (cmd, opts = { stdio: 'inherit' }) => {
   const [file, ...args] = cmd.split(/\s+/);
   return execa(file, args, opts);
